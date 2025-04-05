38ea725d5bd8140925d01f2a11d9f8c7cf00eff1
Mat-eiki <mekreiki@gmail.com>
2025-04-05 16:14:21 -0300

    fixed

---
+++ packages/Pessoa/nometel.py
@@ -0,0 +1,10 @@
+class Pessoa():
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    
+    def telefone(self):
+        print("seu telefone é \n",self.a)
+        
+    def nome(self):
+        print("seu  nome é \n",self.b)
