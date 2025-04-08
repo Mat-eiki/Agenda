ecbe59b2f1979e5330e7a3470d0264ed6b7264d6
Fofodoido <rodrigohenriquedonato@gmail.com>
2025-04-08 12:59:58 -0300

    mudar

--- Agenda/packages/Agenda/metodos.py
+++ Agenda/packages/Agenda/metodos.py
@@ -2,9 +2,11 @@ from packages.Pessoa.nometel import Pessoa
 
 class Agenda():
     
-    def __init__(self,c,d):
-        self.c=c
-    
+    def __init__(self,prim,seg,ter,qua):
+        self.prim=prim
+        self.seg=seg
+        self.ter=ter
+        self.qua=qua
     def loop(self):
         for i in range(self.count):
             print(i)
--- Agenda/testbench_01.py
+++ Agenda/testbench_01.py
@@ -2,10 +2,11 @@ from packages.Pessoa.nometel import Pessoa
 from packages.Agenda.metodos import Agenda
 
 def workspace():
-    entrada = input("Por favor, insira o primeiro nome e telefone, respectivamente, separados por vírgula: ")
+    entrada = input("Por favor, insira o primeiro nome e telefone, respectivamente, separados por um espaço: ")
     nome, telefone = entrada.split(" ")
     confirmar=Pessoa(nome,telefone)
     confirmar.dados()
+
 if __name__=="__main__":
     workspace()
     
