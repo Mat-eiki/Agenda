1a8964e36423656f41bc18d6731081a4142afeb4
Mat-eiki <mekreiki@gmail.com>
2025-04-14 18:18:10 -0300

    arquivos

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
 import Metodos.Add as func
 import Metodos.Rmv as func
+import Metodos.edit as func
+import Metodos.list as func
 
 contatos= {}
-print("Agenda de contatos em python \n Para começar digite um comando: \n Add [nome] [número] \n Rmv [nome] ou [número] \n Edit [nome] [novo nome] ou [número] [novo número] \n List (lista de contatos salvos)")
+print("Agenda de contatos em python \n Para começar digite um comando: \n add [nome] [número] \n rmv [nome] ou [número] \n edit [nome] [novo nome] ou [número] [novo número] \n list (lista de contatos) \n stop (finaliza o programa)")
 
 comando = ""
 while comando != "stop":
     comando = input()
     comando, nome, numero = comando.split()
-    if comando == "Add":
+    if comando == "add":
         func.Adicionar(contatos, nome, numero) 
-    elif comando == "Rmv":
+    elif comando == "rmv":
         func.Remover(contatos, nome, numero)
+    elif comando == "edit":
+        func.editar()
+    elif comando == "list":
+        func.lista(contatos[nome])
